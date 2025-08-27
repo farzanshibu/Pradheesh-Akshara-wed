e50813a6e586aa903b8ee90fda641d832d32ba1a
Viswajit PS <viswajit@hyre.com>
2025-08-27 17:43:57 +0530

    Update deploy script for Windows compatibility and improve cleanup commands

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,14 +1,25 @@
-#!/bin/sh
-# Deploy Vite build to gh-pages branch
+@echo off
+REM Deploy Vite build to gh-pages branch
 set -e
 
+REM Build the project
 npm run build
+
+REM Go to dist folder
 cd dist
+
+REM Initialize git and switch to gh-pages branch
 git init
 git checkout -b gh-pages
+
+REM Add and commit files
 git add .
 git commit -m "Deploy to GitHub Pages"
-git remote add origin https://github.com/VISWAJIT-PS/Pradheesh-Akshara-wed.git
+
+REM Add remote and push to gh-pages branch
+git remote add origin https://github.com/Pradheesh-Akshara-wed.git
 git push -f origin gh-pages
+
+REM Clean up
 cd ..
-rm -rf dist/.git
+rmdir /s /q dist\.git
